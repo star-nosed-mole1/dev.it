d97c3c870d3368866fc0c1286292f570e76a84c3
t1ho <99052158+t1ho@users.noreply.github.com>
2022-07-15 14:35:44 -0500

    Uploaded blank webpack files

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+const config = {
+  mode: process.env.NODE_ENV,
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    'static': {
+      directory: './dist'
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+       test: /\.jsx?/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
