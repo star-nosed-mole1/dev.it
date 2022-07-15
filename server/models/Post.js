76d6957e1701c3cfaca59a014fee3ea89bc5b955
BadWithNames <72165737+BadWithNames@users.noreply.github.com>
2022-07-15 19:26:36 -0400

    Backend

---
+++ server/models/Post.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+  {
+    author_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    // subveddit: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: 'Subveddit',
+    //   required: true,
+    // },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
+    ],
+  },
+  { collection: 'posts' },
+);
+
+module.exports = mongoose.model('Post', postSchema);
---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    joined_on: {
+      type: Date,
+      default: Date.now,
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
+    ],
+  },
+  { collection: 'users' },
+);
+
+module.exports = mongoose.model('User', userSchema);
