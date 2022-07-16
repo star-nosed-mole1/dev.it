5cbce752919ecc8bf3efd9a9ba8d2e8a867d862d
swittuthcoding <swittuthcoding@gmail.com>
2022-07-16 11:12:38 -0400

    made changes

--- server/index.js
+++ server/index.js
@@ -12,35 +12,37 @@ const commentRouter = require("./routes/commentRouter");
 app.use(express.json());
 
 app.use("/user", userRouter);
-app.use("/post", postRouter), app.use("/comment", commentRouter);
+app.use("/post", postRouter);
+app.use("/comment", commentRouter);
 
-// Create subdevit
-app.post("/sub/new", async (req, res, next) => {
-  const { id, name } = req.body;
-  const sub = await Subveddit.create({ created_by: id, name });
-  const user = await User.findById(id);
-  sub.subscribers.push(user);
-  res.send(sub);
-});
+// // Create subdevit
+// app.post("/sub/new", async (req, res, next) => {
+//   const { id, name } = req.body;
+//   const sub = await Subveddit.create({ created_by: id, name });
+//   const user = await User.findById(id);
+//   sub.subscribers.push(user);
+//   res.send(sub);
+// });
 
-// Get subveddit subscribers
-app.get("/sub/:id/subscribers", async (req, res, next) => {
-  // try {
-  //   const { sub_id } = req.body;
-  //   const sub = await Subveddit.findById(sub_id);
-  //   res.send(sub);
-  // } catch (err) {
-  //   return next(err);
-  // }
-  const { id } = req.params;
-  const sub = await Subveddit.findById(id);
-  res.send(sub);
-});
+// // Get subveddit subscribers
+// app.get("/sub/:id/subscribers", async (req, res, next) => {
+//   // try {
+//   //   const { sub_id } = req.body;
+//   //   const sub = await Subveddit.findById(sub_id);
+//   //   res.send(sub);
+//   // } catch (err) {
+//   //   return next(err);
+//   // }
+//   const { id } = req.params;
+//   const sub = await Subveddit.findById(id);
+//   res.send(sub);
+// });
 
 // console.log(typeof process.env.MONGO_DB);
 
-mongoose.connect("process.env.MONGO_DB", () =>
-  console.log("Connected to Mongo DB")
+mongoose.connect(
+  "MONGO_DB=mongodb+srv://carmencarmen:wmPqLPdW1u7nxr2k@cluster0.4fpu4.mongodb.net/devit?retryWrites=true&w=majority",
+  () => console.log("Connected to Mongo DB")
 );
 
 app.listen(3000, () => console.log("Listening on port 3000"));
