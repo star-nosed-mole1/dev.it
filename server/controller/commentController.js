3a652a8e337945785c01d8c028f9d02327a37ff1
Jgarber2675 <jgarber2675@gmail.com>
2022-07-15 21:37:18 -0400

    files have been ressurected

---
+++ server/controller/commentController.js
@@ -0,0 +1,34 @@
+const Comment = require('../models/Comment');
+
+
+const commentController = {};
+
+
+commentController.newComment = async (req, res, next) => {
+    const { author_id, post_id, content } = req.body;
+    const comment = await Comment.create({ author_id, post_id, content });
+    const post = await Post.findById(post_id);
+  
+    post.update({
+      $push: {
+        comments: comment,
+      },
+    });
+  
+    res.send(comment);
+};
+
+commentController.getComments = async (req, res, next) => {
+    try {
+      const { post_id } = req.params;
+      const comments = await Comment.find({ post_id });
+      res.send(comments);
+    } catch (err) {
+      return next(err);
+    }
+    
+};
+
+
+
+module.exports = commentController;
---
+++ server/controller/postController.js
@@ -0,0 +1,37 @@
+const Post = require('../models/Post');
+
+
+const postController = {};
+
+
+postController.createPost = async (req, res, next) => {
+    
+  try {
+      const { id, title, content } = req.body;
+      const post = await Post.create({ author_id: id, title, content });
+      res.send(post);
+    } catch (err) {
+      return next(err);
+  }
+}
+
+postController.getPosts = async (req, res, next) => {
+  if (req.params.user_id) {
+    const { id } = req.params;
+    const posts = await Post.find({ author_id: id });
+    res.send(posts);
+  } else {
+    const posts = await Post.find({});
+  res.send(posts);
+  }
+  
+};
+
+postController.getPostsByPostID = async (req, res, next) => {
+  const { id } = req.params.post_id;
+  const posts = await Post.findById( id );
+  res.send(posts);  
+};
+
+
+module.exports = postController;
