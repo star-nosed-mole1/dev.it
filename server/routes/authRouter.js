bd910e54e96638dff266cfd50d7b20274b58d47c
Jgarber2675 <jgarber2675@gmail.com>
2022-07-16 13:34:50 -0400

    changed authController

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const githubStrategy = require('passport-github2');
 const User = require('../models/User');
-const key = require('./keys');
+const key = require('./key');
 
 const authController = require('../controller/authController');
 
@@ -12,18 +12,18 @@ passport.use(new githubStrategy({
   clientID: key.github.clientID,
   clientSecret:  key.github.clientSecret,
   callbackURL: "http://localhost:3000/auth/github/callback"
-}), (accessToken,refreshToken, profile, done) => {
+}, (accessToken,refreshToken, profile, done) => {
   User.findOrCreate({githubID: profile.id}, (err, user) =>{
     return done(err,user);
   });
-});
+}));
 
 router.get('/login', authController.login);
 router.get('/github',passport.authenticate('github',{scope: ['profile']}));
 
 router.get('github/callback',passport.authenticate('github'),(req,res) => {
   res.send('you have authenticated');
-})
+});
 
 router.get('/logout')
 
