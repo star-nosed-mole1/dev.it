49928a794dbaf9fa09b5a85442dc6529a1b322a7
BadWithNames <72165737+BadWithNames@users.noreply.github.com>
2022-07-15 21:52:23 -0400

    Backend

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -1,19 +1,16 @@
 const Post = require('../models/Post');
 
-
 const postController = {};
 
-
 postController.createPost = async (req, res, next) => {
-    
   try {
-      const { id, title, content } = req.body;
-      const post = await Post.create({ author_id: id, title, content });
-      res.send(post);
-    } catch (err) {
-      return next(err);
+    const { id, title, content } = req.body;
+    const post = await Post.create({ author_id: id, title, content });
+    res.send(post);
+  } catch (err) {
+    return next(err);
   }
-}
+};
 
 postController.getPosts = async (req, res, next) => {
   if (req.params.user_id) {
@@ -22,16 +19,14 @@ postController.getPosts = async (req, res, next) => {
     res.send(posts);
   } else {
     const posts = await Post.find({});
-  res.send(posts);
+    res.send(posts);
   }
-  
 };
 
 postController.getPostsByPostID = async (req, res, next) => {
   const { id } = req.params.post_id;
-  const posts = await Post.findById( id );
-  res.send(posts);  
+  const posts = await Post.findById(id);
+  res.send(posts);
 };
-
 
 module.exports = postController;
--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -1,21 +1,17 @@
 const express = require('express');
 
-const postController = require('../controllers/postController')
-const commentController = require('../controllers/commentController')
+const postController = require('../controller/postController');
+const commentController = require('../controller/commentController');
 
 const router = express.Router();
 
-
-router.post('/',postController.createPost);
+router.post('/', postController.createPost);
 
 router.get('/all', postController.getPosts);
 
 //get comments of specific post
 router.get('/:post_id/comments', commentController.getComments);
 
-
-router.get('/:post_id',postController.getPostsByPostID);
-
-
+router.get('/:post_id', postController.getPostsByPostID);
 
 module.exports = router;
