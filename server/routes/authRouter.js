19e4a8bb5b4867d5a621a5fa63c2c3e17cbb8d23
Jgarber2675 <jgarber2675@gmail.com>
2022-07-16 12:28:36 -0400

    sending back user info

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const githubStrategy = require('passport-github2');
+const User = require('../models/User');
+const key = require('./keys');
 
 const authController = require('../controller/authController');
 
 const router = express.Router();
 
+passport.use(new githubStrategy({
+  clientID: key.github.clientID,
+  clientSecret:  key.github.clientSecret,
+  callbackURL: "http://localhost:3000/auth/github/callback"
+}), (accessToken,refreshToken, profile, done) => {
+  User.findOrCreate({githubID: profile.id}, (err, user) =>{
+    return done(err,user);
+  });
+});
 
 router.get('/login', authController.login);
+router.get('/github',passport.authenticate('github',{scope: ['profile']}));
+
+router.get('github/callback',passport.authenticate('github'),(req,res) => {
+  res.send('you have authenticated');
+})
+
+router.get('/logout')
 
 module.exports = router;
