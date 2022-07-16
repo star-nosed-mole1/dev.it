9697b2677490c805621359391344c64bae0edf2a
BadWithNames <72165737+BadWithNames@users.noreply.github.com>
2022-07-16 10:06:00 -0400

    Updated User model

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -4,12 +4,19 @@ const userController = {};
 
 userController.registerUser = async (req, res, next) => {
   try {
-    const { username, password } = req.body;
-    const user = await User.create({ username, password });
+    const { username, password, avatar } = req.body;
+    const user = await User.create({ username, password, avatar });
     res.send(user);
   } catch (err) {
     return next(err);
   }
+};
+
+userController.getUsers = async (req, res, next) => {
+  const users = await User.find({});
+  //for each obj in users, curr = user.id
+  users.forEach((curr) => (curr = curr._id));
+  res.send(users);
 };
 
 module.exports = userController;
