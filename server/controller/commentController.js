ccc9b8bf98e9e1dd2ca8276f0525ff9dceef1f72
swittuthcoding <swittuthcoding@gmail.com>
2022-07-18 15:27:21 -0400

    changes

--- server/controller/commentController.js
+++ server/controller/commentController.js
@@ -1,7 +1,6 @@
-const Comment = require('../models/Comment');
-const Post = require('../models/Post');
-const User = require('../models/User');
-
+const Comment = require("../models/Comment");
+const Post = require("../models/Post");
+const User = require("../models/User");
 
 const commentController = {};
 
@@ -22,18 +21,10 @@ commentController.newComment = async (req, res, next) => {
 };
 
 commentController.deleteComment = async (req, res, next) => {
-  console.log("test");
   try {
     const { comment_id } = req.params;
-    const { author_id } = req.body;
-    const post = await Post.findOne({ author_id });
-    const user = await User.findById(author_id);
 
-    post.comments.pull({ _id: comment_id });
-    user.comments.pull({ _id: comment_id });
-
-    post.save();
-    user.save();
+    await Comment.findOneAndDelete({ _id: comment_id });
     res.sendStatus(200);
   } catch (err) {
     return next(err);
